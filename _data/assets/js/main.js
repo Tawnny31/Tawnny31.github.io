20835851c388adc0e50561a8463a29530fc2ebec
Tawnny Elizondo Ruiz <105888154+Tawnny31@users.noreply.github.com>
2025-09-04 19:08:21 -0600

    Create main.js

---
+++ _data/assets/js/main.js
@@ -0,0 +1,13 @@
+// Typed.js
+var typed = new Typed('#typed', {
+  strings: ["Hola, soy Tawnny", "Desarrolladora y creativa"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true
+});
+
+// AOS
+AOS.init({
+  duration: 1000,
+  once: true
+});
